09aa561b4201b02fe1abadb97e064d1d92fb3e88
nikit <nikitakur1993@gmail.com>
2014-04-19 17:05:06 +0400

    Добавлена функция инициализации сообщения

--- pa1/main.c
+++ pa1/main.c
@@ -10,13 +10,31 @@
 #include <getopt.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <malloc.h>
+#include <time.h>
 #include "ipc.h"
 #include "common.h"
 #include "pa1.h"
 #include "pipe.h"
 
-void handle_child(const size_t local_id) __attribute__((noreturn));
+/*
+ * Определяет действия дочернего процесса. Данная
+ * функция не должна возвращать управление вызывающему коду,
+ * а должна завершать выполнение процесса
+ *
+ * @param id_proc локальный идентификатор процесса
+ */
+void handle_child(const local_id id_proc) __attribute__((noreturn));
+
+/*
+ * Производит инициализацию сообщения указанного типа,
+ * заполняя поля нужными значениями
+ *
+ * @param msg выделенная память под сообщение
+ * @param type тип сообщения
+ */
+void init_message(Message * const msg, const char * const line, const MessageType type);
 
 extern char * optarg;
 local_id my_local_id;
@@ -38,18 +56,32 @@ int main(int argc, char ** argv) {
 
 	for (size_t i = 0; i < num_proc; i++) {
 		switch (pid = fork()) {
-		case -1: _exit(-1);
-		case 0: handle_child(i); break;
+		case -1:
+			_exit(-1);
+		case 0:
+			handle_child(i);
+			break;
 		default:
 			break;
 		}
 	}
 }
 
-void handle_child(const size_t _local_id) {
+void handle_child(const local_id _local_id) {
 	my_local_id = _local_id;
 
+	// дочерний процесс закрывает ненужные дескрипторы каналов
 	configure_pipes(my_local_id);
 
 	_exit(0);
+}
+
+void init_message(Message * const msg, const char * const line, const MessageType type) {
+	MessageHeader header;
+	header.s_local_time = (timestamp_t) time(NULL); // время создания сообщения
+	header.s_magic = MESSAGE_MAGIC; // магическое число по заданию
+	strcpy(msg->s_payload, line);
+	header.s_payload_len = strlen(line); // текст сообщения
+	header.s_type = type;
+	msg->s_header = header;
 }
