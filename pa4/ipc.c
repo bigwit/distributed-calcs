010421fe7a8e8a5c901ff53093e27d7fd0c51250
nikit <nikit@ubuntu-linux>
2014-05-30 03:27:23 +0400

    Добавлены логические часы

--- pa4/ipc.c
+++ pa4/ipc.c
@@ -8,8 +8,10 @@
 #include <malloc.h>
 #include <string.h>
 #include <unistd.h>
+
 #include "ipc.h"
 #include "pipe.h"
+#include "lamport.h"
 
 /* локальный идентификатор процесса */
 extern local_id my_local_id;
@@ -19,10 +21,12 @@ extern size_t num_proc;
 
 int send(void * self, local_id dst, const Message * msg) {
 	int pipedes = get_write(my_local_id, dst);
-	int bytes = 0;
-	if (pipedes <= 0 || (bytes = write(pipedes, msg, sizeof(Message))) <= 0) {
+	if (pipedes < 0) {
 		return -1;
 	}
+	while (write(pipedes, msg,
+			sizeof(MessageHeader) + msg->s_header.s_payload_len) <= 0)
+		;
 	return 0;
 }
 
@@ -41,54 +45,55 @@ int receive(void * self, local_id from, Message * msg) {
 	int pipedes = get_read(my_local_id, from);
 	// сообщения ожидаются только от дочернего процесса,
 	// родительский только принимает сообщения
-	if (pipedes <= 0) {
-		return -1;
-	}
-	int bytes = read(pipedes, msg, sizeof(Message));
-	if (bytes == -1) {
-		return -1;
-	}
+	while (read(pipedes, &msg->s_header, sizeof(MessageHeader)) <= 0);
+	while (read(pipedes, &msg->s_payload, msg->s_header.s_payload_len) == -1);
+	set_lamport_time(msg->s_header.s_local_time);
 	return 0;
 }
 
 int receive_any(void * self, Message * msg) {
-	// обходим все дочерние процессы, не затрагивая родительский
-	for (local_id id_from = 1; id_from < num_proc; id_from++) {
-		if (id_from != my_local_id) {
-			// и ожидаем сообщения от них
-			if (receive(self, id_from, msg) == -1) {
-				return -1;
-			} else {
-				return 0;
-			}
+	int status = -1;
+	local_id from;
+	// обходим все процессы, включая родительский
+	for (from = 1; status <= 0; from = (from < num_proc) ? from + 1 : 0) {
+		// не затрагивая себя самого
+		if (from == my_local_id) {
+			continue;
+		}
+
+		// получаем дескриптор канала на чтение
+		int pipe_fd = get_read(my_local_id, from);
+		// и ожидаем сообщения от них
+		if (read(pipe_fd, &msg->s_header, sizeof(MessageHeader)) > 0) {
+			*((local_id *) self) = from;
+
+			while (read(pipe_fd, msg->s_payload, msg->s_header.s_payload_len) == -1);
+			set_lamport_time(msg->s_header.s_local_time);
+			return 0;
 		}
 	}
-	return 0;
+	return -1;
 }
 
-int wait_all(const MessageType type) {
-	// хранит информацию о факте получения сообщения от
-	// дочерних процессов - 0 - сообщение не получено,
-	// 1 - сообщение получено
-	char * status = calloc(num_proc, sizeof(char));
-	// сразу исключаем принятие сообщений от родительского и текущего процесса
-	status[0] = 1;
-	status[my_local_id] = 1;
+int wait_all(const MessageType type, int * excludes) {
 	// выделяем память под сообщение для сравнения типов
-	Message * msg = calloc(1, sizeof(Message));
-	for (int i = 1; i < num_proc; i++) {
-
-		if (i == my_local_id) {
+	Message msg;
+	for (local_id i = 1; i < num_proc; i++) {
+		if (i == my_local_id || (excludes != NULL && excludes[i] != 0)) {
 			continue;
 		}
 		// получаем сообщение от процесса
-		memset(msg, 0, sizeof(Message));
-		int result = receive(NULL, i, msg);
+		memset(&msg, 0, sizeof(msg));
+		int result = receive(NULL, i, &msg);
 		if (result != 0) {
 			return -1;
 		}
+		// проверяем, что пришедшее сообщение того же типа, что и ожидалось
+		if (msg.s_header.s_type != type) {
+			fprintf(stderr, "expected type %d, but received %d\n", type,
+					msg.s_header.s_type);
+			return -1;
+		}
 	}
-	free(status);
-	free(msg);
 	return 0;
 }
